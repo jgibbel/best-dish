a309b6fced0fe0c5b23380a736784f59b90398ee
Alex Mendes <mendes.develop@gmail.com>
2019-11-07 14:53:22 -0500

    Google Map react up and running

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 
-function App() {
+
+function Map(){
+  //Create default canvas usind GoogleMap component and stablish default values
+ return (
+   <GoogleMap
+     defaultZoom={13}
+     defaultCenter={{ lat: 40.768520, lng: -73.9559102 }}
+   />
+ );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  //Embed WrappedMap variable and set default props
   return (
-    <div>
-
+    <div style={{width: '100vw', height: '100vw'}}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+        loadingElement={<div style={{ height: '100%'}} />}
+        containerElement={<div style={{ height: '100%'}} />}
+        mapElement={<div style={{height: '100%'}} />}
+      />
     </div>
   );
 }
-
-export default App;
