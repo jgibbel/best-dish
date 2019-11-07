87c8c1a50396a6c15ee03bb9c814b748697a85c1
Alex Mendes <mendes.develop@gmail.com>
2019-11-07 17:18:52 -0500

    Markers and info Window

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+// import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+import { WrappedMap } from './components/Map'
+import { data } from './data'
 
+export default class App extends React.Component {
 
-function Map(){
-  //Create default canvas usind GoogleMap component and stablish default values
- return (
-   <GoogleMap
-     defaultZoom={13}
-     defaultCenter={{ lat: 40.768520, lng: -73.9559102 }}
-   />
- );
-}
+  state = {
+    restaurants: null
+  }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+  componentDidMount(){
+    this.setState({
+      restaurants: data
+    }, () => console.log(this.state.restaurants))
+  }
 
-export default function App() {
   //Embed WrappedMap variable and set default props
-  return (
-    <div style={{width: '100vw', height: '100vw'}}>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: '100%'}} />}
-        containerElement={<div style={{ height: '100%'}} />}
-        mapElement={<div style={{height: '100%'}} />}
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div style={{width: '100vw', height: '100vw'}}>
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          loadingElement={<div style={{ height: '100%'}} />}
+          containerElement={<div style={{ height: '100%'}} />}
+          mapElement={<div style={{height: '100%'}} />}
+        />
+      </div>
+    );
+  }
+
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,1 +1,57 @@
+import React, { useState } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps'
+import { data } from '../data'
 
+function Map(){
+  const [selectedPark, setSelectedPark] = useState(null)
+
+ return (
+   <GoogleMap
+     defaultZoom={10}
+     defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+    >
+      { data.map(park => (< Marker
+        key={park.properties.PARK_ID}
+        position={{
+          lat: park.geometry.coordinates[1],
+          lng: park.geometry.coordinates[0]
+        }}
+        onClick={ () => {
+          setSelectedPark(park)
+        }}
+        />
+      ))}
+
+      { selectedPark && (
+        < InfoWindow
+        position={{
+          lat: selectedPark.geometry.coordinates[1],
+          lng: selectedPark.geometry.coordinates[0]
+        }}
+        onCloseClick={() => setSelectedPark(null) }
+        >
+          <div>
+            <h2>Restaurant Name</h2>
+            <p>Restaurant Address</p>
+            <p>Restaurant Price</p>
+            <button>Click me!</button>
+
+          </div>
+
+
+        </InfoWindow>
+      )}
+
+   </GoogleMap>
+ );
+}
+
+export const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+
+
+// Create default canvas usind GoogleMap component and stablish default values
+// Create WrappedMap variable and assign it to a function imported from 'react-google-maps'
+// withScriptjs() takes withGoogleMap() as a callback wich has the return of Map()
+// import Marker and change its attributes based on each choice
+// /* Use of hooks*/
