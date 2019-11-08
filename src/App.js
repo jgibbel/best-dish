8107c91b1c01c919e312774707bd416a4e876bf8
jgibbel <jamesgibbel@gmail.com>
2019-11-08 17:34:58 -0500

    merge issue

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends React.Component {
     return (
       <div style={{width: '100vw', height: '100vw'}}>
         <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=`}
           loadingElement={<div style={{ height: '100%'}} />}
           containerElement={<div style={{ height: '100%'}} />}
           mapElement={<div style={{height: '100%'}} />}
